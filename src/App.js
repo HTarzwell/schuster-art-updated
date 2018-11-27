3d8fb19655dde513f6d3c37a385d372e9d1a2bc6
HTarzwell <htarzwell@gmail.com>
2018-11-27 17:07:04 -0500

    Added 3 more components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Navbar/Navbar'
+import Carousel from './Carousel/Carousel'
+import About from './About/About'
+import Artists from './Artists/Artists'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Carousel />
+        <About />
+        <Artists />
         <header className="App-header">
           <h1>Schuster Art Consultancy</h1>
         </header>
---
+++ src/Artists/Artists.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Artists extends React.Component {
+  render() {
+    return (
+      <div className="artists">
+        <h1>This is the ARTISTS component!</h1>
+      </div>
+    )
+  }
+}
+
+export default Artists
