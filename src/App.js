b316403e9310fa7073e96bee31b01d4c3a392440
HTarzwell <htarzwell@gmail.com>
2020-09-29 11:13:01 -0400

    updating npm and app structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from './Navbar/Navbar'
-// import Carousel from './Carousel/Carousel'
 import About from './About/About'
 import Artists from './Artists/Artists'
 import Contact from './Contact/Contact'
+import Favorites from './Favorites/Favorites'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
 
         <About />
         <Artists />
         <Contact />
+        <Favorites />
         <header className="App-header">
           <h1>Schuster Art Consultancy</h1>
         </header>
