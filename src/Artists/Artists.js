27a2f4dd7cf730836af6f790082648bbfd43aa7d
HTarzwell <htarzwell@gmail.com>
2020-09-30 17:31:54 -0400

    front end functionality basics

--- src/Artists/Artists.js
+++ src/Artists/Artists.js
@@ -9,17 +9,20 @@ class Artists extends React.Component {
       {
         original: 'https://picsum.photos/id/1018/1000/600/',
         thumbnail: 'https://picsum.photos/id/1018/250/150/',
+        description: 'artist 1'
       },
       {
         original: 'https://picsum.photos/id/1015/1000/600/',
         thumbnail: 'https://picsum.photos/id/1015/250/150/',
+        description: 'artist 2'
       },
       {
         original: 'https://picsum.photos/id/1019/1000/600/',
         thumbnail: 'https://picsum.photos/id/1019/250/150/',
+        description: 'artist 3'
       },
     ];
-    
+
     return (
       <div className="artists">
         <h1>This is the ARTISTS component!</h1>
--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,7 +4,24 @@ class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
-        <h1>This is the contact div!</h1>
+        <div className="contact-form">
+          <h1>Interested in learning more?</h1>
+          <label>First Name</label>
+          <input type="text"/>
+          <label>Last Name</label>
+          <input type="text" />
+          <label>Email Address</label>
+          <input type="email" />
+          <label>Subject</label>
+          <input type="text" />
+          <label>Message</label>
+          <input type="text" />
+          <label>Sign Up For Our Newsletter</label>
+          <input type="checkbox" />
+        </div>
+        <div className="contact-image">
+          <img src={require('../About/founder_img.png')} alt='i am so sorry'/>
+        </div>
       </div>
     )
   }
