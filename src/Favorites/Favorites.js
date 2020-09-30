eebaabe7dc8bd7393527676eb974c3c8b96b3a7b
HTarzwell <htarzwell@gmail.com>
2020-09-30 15:04:15 -0400

    updated files

---
+++ src/Favorites/Favorites.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Favorites extends React.Component {
+  render() {
+    return (
+      <div className="favorites">
+        <h1>This is the favorites div!</h1>
+      </div>
+    )
+  }
+}
+
+export default Favorites
---
+++ src/Main.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './Main.css';
+import About from './About/About'
+import Artists from './Artists/Artists'
+import Contact from './Contact/Contact'
+import Favorites from './Favorites/Favorites'
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
+class Main extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div className="App">
+          <div className="App-header">
+            <header>
+              <h1>Schuster Art Consultancy</h1>
+            </header>
+            <ul>
+              <li><NavLink to="/">Artists</NavLink></li>
+              <li><NavLink to="/about">About</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+              <li><NavLink to="/favorites">Favorites</NavLink></li>
+            </ul>
+          </div>
+          <div className="App-content">
+            <Route exact path="/" component={Artists}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+            <Route path="/favorites" component={Favorites}/>
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default Main;
